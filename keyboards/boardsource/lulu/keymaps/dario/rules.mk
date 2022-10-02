e3794835136f539e166cf9f7b3dcee7b47d55148
daranguiz <dario@hey.com>
2022-10-01 20:39:15 -0600

    initial lulu commit

---
+++ keyboards/boardsource/lulu/keymaps/dario/rules.mk
@@ -0,0 +1,10 @@
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
+OLED_ENABLE = yes
+SPLIT_KEYBOARD = yes
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+
+COMBO_ENABLE = yes
+LTO_ENABLE = yes
+
+SRC += ./mods.c \
+       ./features/caps_word.c
